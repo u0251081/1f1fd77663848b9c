c350ecd2a44d90afc2ca73c252d823fa5663cb2e
u2051081 <u0251081@nkfust.eu.tw>
2018-06-29 16:48:55 +0800

    fix payment

--- testpay.php
+++ testpay.php
@@ -6,20 +6,17 @@ sql();
 include_once('AllPay.Payment.Integration.php');
 
 //這邊寫入consumer_order-----//
-if(isset($_SESSION["member_no"]))
-{
-    @$member_id = $_SESSION["member_no"];
-}
-else
-{
-    @$member_id = $_SESSION["fb_id"];
+if(isset($_SESSION["member_no"])) {
+    $member_id = $_SESSION["member_no"];
+} else {
+    $member_id = $_SESSION["fb_id"];
 }
 
 //----------從購物車來的資料-------//
-@$cart_pid = $_POST['cart_pid']; //格式：1,2,3,
-@$cart_qty = $_POST['cart_qty']; //格式：1,2,3,
-@$cart_price = $_POST['cart_price']; //格式：1,2,3,
-@$p_name_ary = substr($_POST['p_name_ary'],0,-1); //從購物車購買
+$cart_pid = $_POST['cart_pid']; //格式：1,2,3,
+$cart_qty = $_POST['cart_qty']; //格式：1,2,3,
+$cart_price = $_POST['cart_price']; //格式：1,2,3,
+$p_name_ary = substr($_POST['p_name_ary'],0,-1); //從購物車購買
 //-------------------------------//
 
 //----------單筆資料--------------//
@@ -47,7 +44,7 @@ $order_id = time(); //訂單編號產生
 @$share_vip_id = $_SESSION['share_vip_id']; //導購處裡->vip分享
 if(@$_POST['fb_no'] == "")
 {
-   @$fb_no = 0;
+    @$fb_no = 0;
 
 }
 else
@@ -79,8 +76,11 @@ if(!empty($p_id) && !empty($pay_qty))
     //如果只有行銷經理的值代表行銷經理分享
     if($manager_id != "" && $vip_id == '' && $manager_id != $_SESSION['manager_no'])
     {
-        $sql = "INSERT INTO consumer_order SET order_no='$order_id', m_id='$member_no',fb_no='$fb_no', pay_type='$paymentchose', pay_time='".date('Y-m-d H:i:s')."',
-    o_price='$TotalAmount', order_time='".date('Y-m-d H:i:s')."', is_effective='0'";
+        $sql = "";
+        $sql .= "INSERT INTO consumer_order SET";
+        $sql .= " order_no='$order_id', m_id='$member_no',fb_no='$fb_no',";
+        $sql .= " pay_type='$paymentchose', pay_time='".date('Y-m-d H:i:s')."',";
+        $sql .= " o_price='$TotalAmount', order_time='".date('Y-m-d H:i:s')."', is_effective='0'";
         mysql_query($sql);
         $insert_id = mysql_insert_id();
         if($insert_id != "")
@@ -463,11 +463,11 @@ function create_order($p_name,$pay_time,$TotalAmount,$web_price,$order_id,$share
         $oPayment = new AllInOne();
         /* 服務參數 */
         $oPayment->ServiceURL = "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5"; //測試環境網址，正式版的話要改為正式環境
-       $oPayment->HashKey = "8zRmJYvEXQ2HpHqu";//介接測試 (Hash Key)這是測試帳號專用的不用改它
+        $oPayment->HashKey = "8zRmJYvEXQ2HpHqu";//介接測試 (Hash Key)這是測試帳號專用的不用改它
         $oPayment->HashIV = "0Pa3jmkAwsErXh6a";//介接測試 (Hash IV)這是測試帳號專用的不用改它
         $oPayment->MerchantID = "3048337";//特店編號 (MerchantID)這是測試帳號專用的不用改它
 
-         $BackUrl="http://".$_SERVER['HTTP_HOST']."/payment_info.php";
+        $BackUrl="http://".$_SERVER['HTTP_HOST']."/17mai/payment_info.php";
         $BackUrl2="http://".$_SERVER['HTTP_HOST']."/index.php?url=order_search"; //超商繳費用
 
         if(@$share_id != "")
@@ -486,22 +486,22 @@ function create_order($p_name,$pay_time,$TotalAmount,$web_price,$order_id,$share
         $oPayment->Send['TradeDesc'] = "一起購商城購物";//交易敘述
         if($paymentchose=="CVS")
         {
-             $oPayment->Send['ChoosePayment'] = PaymentMethod::CVS;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
-             $oPayment->Send['OrderResultURL'] =$BackUrl3;
+            $oPayment->Send['ChoosePayment'] = PaymentMethod::CVS;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
+            $oPayment->Send['OrderResultURL'] =$BackUrl3;
         }
         else
         {
-             $oPayment->Send['ChoosePayment'] = PaymentMethod::Credit;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
-             $oPayment->Send['OrderResultURL'] = "http://www.17mai.com.tw/index.php?url=order_search";
+            $oPayment->Send['ChoosePayment'] = PaymentMethod::Credit;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
+            $oPayment->Send['OrderResultURL'] = "http://www.17mai.com.tw/index.php?url=order_search";
         }
-       
+
         $oPayment->Send['NeedExtraPaidInfo'] = ExtraPaymentInfo::No; //若不回傳額外的付款資訊時，參數值請傳：Ｎ
         $oPayment->Send['Remark'] = "無備註"; //廠商後台訂單備註
 
         $oPayment->SendExtend['PaymentInfoURL'] = $BackUrl3;
         $oPayment->Send['ReturnURL'] = $BackUrl3; //當消費者付款完成後，歐付寶會將付款結果參數回傳到該網址。
         $oPayment->Send['ClientBackURL'] = $BackUrl2; //消費者點選此按鈕後，會將頁面導回到此設定的網址
-         //為付款完成後，歐付寶將頁面導回到會員網址，並將付款結果帶回
+        //為付款完成後，歐付寶將頁面導回到會員網址，並將付款結果帶回
         //請填入你主機要接受訂單付款後狀態 回傳的程式名稱 記住 該網址需能對外
         //接受訂單狀態 回傳程式名稱 可在此程式內將付款方式寫入你的訂單中 payment_info.php 與 return.php 程式內容一樣
 
@@ -538,21 +538,25 @@ function create_order2($cart_pid,$p_name_ary,$pay_time,$TotalAmount,$cart_price,
     {
         $oPayment = new AllInOne();
         /* 服務參數 */
-       $oPayment->ServiceURL = "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5"; //測試環境網址，正式版的話要改為正式環境
-       $oPayment->HashKey = "8zRmJYvEXQ2HpHqu";//介接測試 (Hash Key)這是測試帳號專用的不用改它
+        $oPayment->ServiceURL = "https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5"; //測試環境網址，正式版的話要改為正式環境
+        $oPayment->HashKey = "8zRmJYvEXQ2HpHqu";//介接測試 (Hash Key)這是測試帳號專用的不用改它
         $oPayment->HashIV = "0Pa3jmkAwsErXh6a";//介接測試 (Hash IV)這是測試帳號專用的不用改它
         $oPayment->MerchantID = "3048337";//特店編號 (MerchantID)這是測試帳號專用的不用改它
 
-        $BackUrl="http://".$_SERVER['HTTP_HOST']."/payment_info.php";
-        $BackUrl2="http://".$_SERVER['HTTP_HOST']."/index.php?url=order_search"; //超商繳費用
+        $dir = "17mai";
+        $BaseUrl = "http://www.17mai.com.tw/";
+        $BaseUrl = "http://".$_SERVER['HTTP_HOST'].'/'.$dir.'/';
+        $BackUrl1 = $BaseUrl."payment_info.php";
+        $BackUrl2 = $BaseUrl."index.php?url=order_search"; //超商繳費用
 
-        if(@$share_id != "")
-        {
-            @$BackUrl3="http://".$_SERVER['HTTP_HOST']."/payment_info.php?id=".$order_id."&p_id=".$cart_pid."&member_no=".$member_no."&pay_qty=".$cart_qty."&share_id=".$share_id; //超商繳費用
-        }
-        else
-        {
-            @$BackUrl3="http://".$_SERVER['HTTP_HOST']."/payment_info.php?id=".$order_id."&p_id=".$cart_pid."&member_no=".$member_no."&pay_qty=".$cart_qty; //超商繳費用
+        $BackUrl3 = "";
+        $BackUrl3 .= $BackUrl1."?";
+        $BackUrl3 .= "id=$order_id&p_id=$cart_pid&member_no=$member_no&pay_qty=$cart_qty"; //超商繳費用
+
+        if(isset($share_id) && $share_id != "") {
+            $BackUrl3 .= "&share_id=$share_id";
+            //超商繳費用
+        } else {
         }
 
         /* 基本參數 */
@@ -560,15 +564,15 @@ function create_order2($cart_pid,$p_name_ary,$pay_time,$TotalAmount,$cart_price,
         $oPayment->Send['MerchantTradeDate'] = $pay_time; //商店交易時間
         $oPayment->Send['TotalAmount'] = (int) $TotalAmount;//付款總金額，超商最低30元起
         $oPayment->Send['TradeDesc'] = "一起購商城購物";//交易敘述
-         if($paymentchose=="CVS")
+        if($paymentchose=="CVS")
         {
-             $oPayment->Send['ChoosePayment'] = PaymentMethod::CVS;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
-             $oPayment->Send['OrderResultURL'] =$BackUrl3;
+            $oPayment->Send['ChoosePayment'] = PaymentMethod::CVS;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
+            $oPayment->Send['OrderResultURL'] =$BackUrl3;
         }
         else
         {
-             $oPayment->Send['ChoosePayment'] = PaymentMethod::Credit;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
-             $oPayment->Send['OrderResultURL'] = "http://www.17mai.com.tw/index.php?url=order_search";
+            $oPayment->Send['ChoosePayment'] = PaymentMethod::Credit;//付自行選款方式 這邊是開啟所有付款方式讓消費者擇
+            $oPayment->Send['OrderResultURL'] = $BackUrl2;
         }
 
         $oPayment->Send['NeedExtraPaidInfo'] = ExtraPaymentInfo::No; //若不回傳額外的付款資訊時，參數值請傳：Ｎ
@@ -587,7 +591,16 @@ function create_order2($cart_pid,$p_name_ary,$pay_time,$TotalAmount,$cart_price,
         // 加入選購商品資料。
         for($i=0; $i<count($cart_pid2); $i++)
         {
-            array_push($oPayment->Send['Items'], array('Name' => $p_name_ary2[$i], 'Price' => (int)$cart_price2[$i], 'Currency' => "元", 'Quantity' => (int)$cart_qty2[$i], 'URL' => "無"));
+            array_push(
+                $oPayment->Send['Items'],
+                array(
+                    'Name' => $p_name_ary2[$i],
+                    'Price' => (int)$cart_price2[$i],
+                    'Currency' => "元",
+                    'Quantity' => (int)$cart_qty2[$i],
+                    'URL' => "無"
+                )
+            );
         }
 
         /* 產生訂單 */
