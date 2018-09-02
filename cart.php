b14fc98390e9193b3252187f92e469b54291fba2
u2051081 <u0251081@nkfust.eu.tw>
2018-09-02 17:37:17 +0800

    購物車功能完成

--- cart.php
+++ cart.php
@@ -1,3 +1,93 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xin-an
+ * Date: 2018/8/29
+ * Time: 上午 03:20
+ */
+
+use function Base17Mai\take;
+
+function getProductFromCart()
+{
+    $SQL = 'select id as CID, productID, Quantity, specCode from shoppingcart where member_no = :member_no;';
+    $Para['member_no'] = take('member_no', '', 'session');
+    $result = pdo_select_sql($SQL, $Para);
+    return $result;
+}
+
+function getProductInformation($ProductList = [])
+{
+    if (is_array($ProductList)) {
+        $result = [];
+        foreach ($ProductList as $value) {
+            $SQL = 'select id as PID, productID, PName, unitPrice, Prelease from product where id = :id;';
+            $para = ['id' => $value['productID']];
+            $rst = pdo_select_sql($SQL, $para);
+            if (isset($rst[0])) {
+                $SQL = 'select specification from productspec where productID = :productID and specCode = :specCode;';
+                $para = ['productID' => $rst[0]['productID'], 'specCode' => $value['specCode']];
+                $rst2 = pdo_select_sql($SQL, $para);
+                $rst[0]['specification'] = isset($rst2[0]) ? $rst2[0]['specification'] : '';
+                $rst[0]['Prelease'] = ($rst[0]['Prelease'] === '1') ? '上架' : '下架';
+                $rst[0] = array_merge($rst[0], $value);
+                $result[] = $rst[0];
+            }
+        }
+        return $result;
+    } else {
+        return false;
+    }
+}
+
+function generateTable($ProductList = [])
+{
+    if (is_array($ProductList)) {
+        $result = '';
+        foreach ($ProductList as $key => $value) {
+            $result .= "<tr id=\"{$value['CID']}\">";
+            $result .= '<td>';
+            $result .= "<a class=\"remove remove_cart\" href=\"javascript:void(0) pid\"{$value['PID']}\">";
+            $result .= "<i class=\"fa fa-close\"></i>";
+            $result .= "</a>";
+            $result .= "</td>";
+            $result .= '<td>';
+            $result .= "<a class=\"aa-cart-title\" href=\"index.php?url=product_detail&id={$value['PID']}\">";
+            $result .= $value['PName'];
+            $result .= "</a>";
+            $result .= "<input type='hidden' name='productID' value='{$value['PID']}'>";
+            $result .= "</td>";
+            $result .= '<td>';
+            $result .= "<a>";
+            $result .= $value['Prelease'];
+            $result .= "</a>";
+            $result .= "</td>";
+            $result .= '<td>';
+            $result .= "<a>";
+            $result .= $value['specification'];
+            $result .= "</a>";
+            $result .= "<input type='hidden' name='specCode' value='{$value['specCode']}'>";
+            $result .= "</td>";
+            $result .= '<td>';
+            $result .= "<input type=\"number\" name=\"Quantity\" value=\"{$value['Quantity']}\" >";
+            $result .= "</td>";
+            $result .= '<td id="price">';
+            $result .= $value['unitPrice'];
+            $result .= "</td>";
+            $result .= '<td id="amount">';
+            $result .= "</td>";
+            $result .= '</tr>';
+        }
+        return $result;
+    }
+}
+
+$shoppingList = getProductFromCart();
+$productList = getProductInformation($shoppingList);
+$cartTable = generateTable($productList);
+
+?>
+
 <!-- 網站位置導覽列 -->
 <section id="aa-catg-head-banner">
     <div class="container">
@@ -20,7 +110,7 @@
             <div class="col-md-12">
                 <div class="cart-view-area" style="margin-bottom: 100px; padding-top: 1px;">
                     <div class="cart-view-table">
-                        <form method="post">
+                        <form id="cartForm" method="post">
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
@@ -28,69 +118,22 @@
                                         <th>取消</th>
                                         <th>名稱</th>
                                         <th>狀態</th>
-                                        <th>金額</th>
+                                        <th>規格</th>
                                         <th>數量</th>
+                                        <th>金額</th>
+                                        <th>小計</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    <?php
-                                    if(isset($_SESSION["member_no"]))
-                                    {
-                                        $member_id = $_SESSION["member_no"];
-                                    }
-                                    else
-                                    {
-                                        $member_id = $_SESSION["fb_id"];
-                                    }
-
-                                    $sql = "SELECT * FROM shoping_cart WHERE m_id='".$member_id."'";
-                                    $res = mysql_query($sql);
-                                    while($row = mysql_fetch_array($res))
-                                    {
-                                        $product = "SELECT * FROM product WHERE id='".$row['p_id']."'";
-                                        $product_res = mysql_query($product);
-                                        $product_row = mysql_fetch_array($product_res);
-                                        $last = $product_row['rem_qty'];
-                                        if($product_row['added'] == 1 && $last > 0)
-                                        {
-                                            ?>
-                                            <tr id="tr<?php echo $row['id']; ?>">
-                                                <td>
-                                                    <a class="remove" href="javascript:void(0)" name="remove_cart" pid="<?php echo $row['id']; ?>">
-                                                        <fa class="fa fa-close"></fa>
-                                                    </a>
-                                                </td>
-                                                <td>
-                                                    <a class="aa-cart-title" href="index.php?url=product_detail&product_id=<?php echo $row['p_id']; ?>">
-                                                        <?php
-                                                        echo $product_row['p_name'];
-                                                        ?>
-                                                    </a>
-                                                    <span name="pid" style="display: none;"><?php echo $row['p_id']; ?></span>
-                                                </td>
-                                                <td><a><?php echo '上架'; ?></a></td>
-                                                <td name="price" id="td<?php echo $row['id']; ?>">
-                                                    <?php
-                                                    $price_sql = "SELECT web_price FROM price WHERE p_id='".$row['p_id']."'";
-                                                    $price_res = mysql_query($price_sql);
-                                                    $price_row = mysql_fetch_array($price_res);
-                                                    echo @$price_row['web_price'];
-                                                    ?>
-                                                </td>
-                                                <td>
-                                                    <input class="aa-cart-quantity" type="number" name="qty" value="<?php echo $row['p_qty']; ?>">
-                                                </td>
-                                            </tr>
-                                            <?php
-                                        }
-                                    }
-                                    ?>
+                                    <?= $cartTable ?>
                                     <tr>
-                                        <td colspan="6" class="aa-cart-view-bottom">
-                                            <div class="aa-cart-coupon">
-                                                <input class="aa-cart-view-btn" type="button" onclick="location.href='index.php'" value="繼續購物">
+                                        <td colspan="7" class="aa-cart-view-bottom">
+                                            <div class="aa-cart-coupon" style="">
+                                                <input class="aa-cart-view-btn" type="button" id="btn1" value="繼續購物">
                                             </div>
-                                            <input class="aa-cart-view-btn" id="btn" type="button" value="修改數量">
+                                            <div class="aa-cart-coupon">
+                                            </div>
+                                            <input class="aa-cart-view-btn" type="button" id="btn2" value="修改數量">
                                         </td>
                                     </tr>
                                     </tbody>
@@ -108,7 +151,7 @@
                                 </tr>
                                 </tbody>
                             </table>
-                            <a href="javascript:void(0)" id="to_pay" class="aa-cart-view-btn">前往結帳</a>
+                            <a href="javascript:void(0)" id="btn3" class="aa-cart-view-btn">前往結帳</a>
                         </div>
                     </div>
                 </div>
@@ -127,97 +170,111 @@
 <!-- / Cart view section -->
 
 <script>
+
+    (function () {
+        refreshAmount();
+    })();
+
+    function getCartData() {
+        let result = {};
+        $('tr[id]').each(function (ind, ele) {
+            let tmpData = {};
+            let element = $(ele);
+            tmpData.productID = String(element.find('input[name="productID"]').val());
+            tmpData.Quantity = String(element.find('input[name="Quantity"]').val());
+            tmpData.specCode = String(element.find('input[name="specCode"]').val());
+            result[ind] = tmpData;
+        });
+        return result;
+    }
+
+    function refreshAmount() {
+        let total = 0;
+        $('tr[id]').each(function (ind, ele) {
+            let element = $(ele);
+            let Quantity = Number(element.find('input[name="Quantity"]').val());
+            let UnitPrice = Number(element.find('td#price').text());
+            let amount = Quantity * UnitPrice;
+            element.find('td#amount').text(amount);
+            total += amount;
+        });
+        $('span#price_total').text(total);
+    }
+
+    $(document).on('click', 'a.remove_cart', function () {
+        let CartID = $(this).closest('tr').attr('id');
+        ajax17mai('Consumer', 'RemoveFromCart', {}, {CID: CartID});
+    });
+    $(document).on('click', 'input#btn1', function () {
+        location.href = "<?= $history ?>";
+    });
+
+    $(document).on('click', 'input#btn2', function () {
+        ajax17mai('Consumer', 'UpdateCart', {}, {cartItem: getCartData()});
+    });
+
+    $(document).on('click', 'a#btn3', function () {
+        location.href = 'index.php?url=pay_check';
+    });
+
     var pid;
     var price_ary = Array();
-    $("td[name='price']").each(function(i)
-    {
-        price_ary[i] = $(this).text().trim();
-    });
 
-    var qty_ary = Array();
-    $("input[name='qty']").each(function(i)
-    {
-        qty_ary[i] = $(this).val().trim();
-    });
-
-    $(function ()
-    {
+    $(function () {
+        // 隱藏廣告欄
         $("#aa-slider").remove();
         $("html,body").scrollTop(70);
     });
 
-    total(price_ary,qty_ary);
-
-    function total(price,qty)
-    {
-        var num = 0;
-        for(var i=0; i<price.length; i++)
-        {
-            num += price[i] * qty[i];
-        }
-        $("#price_total").text(num);
-    }
-
-    $("#btn").click(function()
-    {
+    $("#btn").click(function () {
         var pid_arr = Array();
-        $("span[name='pid']").each(function(i)
-        {
+        $("span[name='pid']").each(function (i) {
             pid_arr[i] = $(this).text().trim();
         });
 
         var qty_arr = Array();
-        $("input[name='qty']").each(function(i)
-        {
+        $("input[name='qty']").each(function (i) {
             qty_arr[i] = $(this).val().trim();
         });
 
         var price_arr = Array();
-        $("td[name='price']").each(function(i)
-        {
+        $("td[name='price']").each(function (i) {
             price_arr[i] = $(this).text().trim(price_arr);
         });
 
         $.ajax
         ({
-            url:"ajax.php", //接收頁
-            type:"POST", //POST傳輸
-            data:{type:"update_cart", pid_arr:pid_arr,qty_arr:qty_arr,price_arr:price_arr}, // key/value
-            dataType:"text", //回傳形態
-            success:function(i) //成功就....
+            url: "ajax.php", //接收頁
+            type: "POST", //POST傳輸
+            data: {type: "update_cart", pid_arr: pid_arr, qty_arr: qty_arr, price_arr: price_arr}, // key/value
+            dataType: "text", //回傳形態
+            success: function (i) //成功就....
             {
-                if(i == 1)
-                {
-                    if($("#device").text() == 'mobile')
-                    {
+                if (i == 1) {
+                    if ($("#device").text() == 'mobile') {
                         window.javatojs.showInfoFromJs('更新成功');
-                        window.location.href='index.php?url=cart';
+                        window.location.href = 'index.php?url=cart';
                     }
-                    else
-                    {
+                    else {
                         alert('更新成功');
-                        window.location.href='index.php?url=cart';
+                        window.location.href = 'index.php?url=cart';
                     }
                 }
             },
-            error:function()//失敗就...
+            error: function ()//失敗就...
             {
                 //alert("ajax失敗");
             }
         });
     });
 
-    $("a[name='remove_cart']").click(function()
-    {
+    $("a[name='remove_cart']").click(function () {
         pid = $(this).attr('pid');
-        if($("#device").text() == 'mobile')
-        {
+        if ($("#device").text() == 'mobile') {
             window.javatojs.myconfirm('cart');
         }
-        else
-        {
-            if(confirm('是否要從您的追蹤清單內移除?'))
-            {
+        else {
+            if (confirm('是否要從您的追蹤清單內移除?')) {
                 var price = $(this).parent().siblings("td[name='price']").text().trim();
                 var qty = $(this).parent().siblings('td').find('input[name="qty"]').val();
                 var price_total = $("#price_total").text();
@@ -225,22 +282,20 @@
                 $("#price_total").text(price_total - num);
                 $.ajax
                 ({
-                    url:"ajax.php", //接收頁
-                    type:"POST", //POST傳輸
-                    data:{type:"remove_cart", pid:pid}, // key/value
-                    dataType:"text", //回傳形態
-                    success:function(i) //成功就....
+                    url: "ajax.php", //接收頁
+                    type: "POST", //POST傳輸
+                    data: {type: "remove_cart", pid: pid}, // key/value
+                    dataType: "text", //回傳形態
+                    success: function (i) //成功就....
                     {
-                        if(i == 1)
-                        {
-                            $("#tr"+pid).remove();
+                        if (i == 1) {
+                            $("#tr" + pid).remove();
                         }
-                        else
-                        {
+                        else {
                             alert('意外的錯誤，請重新操作');
                         }
                     },
-                    error:function()//失敗就...
+                    error: function ()//失敗就...
                     {
                         //alert("ajax失敗");
                     }
@@ -249,30 +304,27 @@
         }
     });
 
-    function dialod_res(t)
-    {
-        if(t == 'yes')
-        {
-            var price = $("a[name='remove_cart']").parent().siblings("td#td"+pid).text().trim();
-            var qty = $("a[name='remove_cart']").parent().siblings("td#td"+pid).next().find('input[name="qty"]').val();
+    function dialod_res(t) {
+        if (t == 'yes') {
+            var price = $("a[name='remove_cart']").parent().siblings("td#td" + pid).text().trim();
+            var qty = $("a[name='remove_cart']").parent().siblings("td#td" + pid).next().find('input[name="qty"]').val();
             var price_total = $("#price_total").text();
             var num = price * qty;
             $("#price_total").text(price_total - num);
             $.ajax
             ({
-                url:"ajax.php", //接收頁
-                type:"POST", //POST傳輸
-                data:{type:"remove_cart", pid:pid}, // key/value
-                dataType:"text", //回傳形態
-                success:function(i) //成功就....
+                url: "ajax.php", //接收頁
+                type: "POST", //POST傳輸
+                data: {type: "remove_cart", pid: pid}, // key/value
+                dataType: "text", //回傳形態
+                success: function (i) //成功就....
                 {
-                    if(i == 1)
-                    {
-                        $("#tr"+pid).remove();
+                    if (i == 1) {
+                        $("#tr" + pid).remove();
                         window.javatojs.showInfoFromJs('已移除購物車');
                     }
                 },
-                error:function()//失敗就...
+                error: function ()//失敗就...
                 {
                     //alert("ajax失敗");
                 }
@@ -280,25 +332,19 @@
         }
     }
 
-    $("#to_pay").click(function()
-    {
+    $("#to_pay").click(function () {
         var p_t = $("#price_total").text().trim();
-        if(p_t <= 0)
-        {
-            if($("#device").text() == 'mobile')
-            {
+        if (p_t <= 0) {
+            if ($("#device").text() == 'mobile') {
                 window.javatojs.showInfoFromJs('購物車內沒有商品');
             }
-            else
-            {
+            else {
                 alert('購物車內沒有商品');
             }
         }
-        else
-        {
+        else {
             var pid_arr = Array();
-            $("span[name='pid']").each(function(i)
-            {
+            $("span[name='pid']").each(function (i) {
                 pid_arr[i] = $(this).text().trim();
             });
             //pid_arr =>商品id陣列
