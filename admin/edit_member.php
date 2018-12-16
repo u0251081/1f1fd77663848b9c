7437e2f7226bafb17932b4b20279629c2af7c798
Xin-an <u0251081@nkfust.edu.tw>
2018-12-16 16:55:47 +0800

    loading and updating member_information update

--- admin/edit_member.php
+++ admin/edit_member.php
@@ -1,5 +1,23 @@
 <?php
-@$id = $_GET['id'];
+
+use Base17Mai\Member, Base17Mai\Bank;
+use function Base17Mai\take;
+
+$id = take('id', '', 'GET');
+if (empty($id)) exit('尚未選擇會員');
+$Member = new Member();
+$Bank = new Bank();
+$Profile = $Member->getMemberProfile(['id' => $id]);
+$Profile['memberID'] = $id;
+$bankList = $Bank->getBankList();
+$bankIDListHtml = $Member->generateBankOptionsHtml($bankList, $Profile['bank_id']);
+$genderList = $Member->getAllGender();
+$genderOptionsHtml = $Member->generateGenderOption($genderList, $Profile['gender']);
+$cityList = $Member->getAllCity();
+$cityOptionsHtml = $Member->generateCityOptionHtml($cityList, $Profile['city_id']);
+$areaList = $Member->getSpecifyArea($Profile['city_id']);
+$areaOptionsHtml = $Member->generateAreaOptionHtml($areaList, $Profile['area_id']);
+$id = $_GET['id'];
 $sql = "SELECT * FROM member WHERE id='$id'";
 $res = mysql_query($sql);
 $row = mysql_fetch_array($res);
@@ -7,98 +25,97 @@ $row = mysql_fetch_array($res);
 <div class="widget">
     <h4 class="widgettitle">修改會員資料</h4>
     <div class="widgetcontent">
-        <form class="stdform stdform2" method="post">
+        <form id="profile_form" class="stdform stdform2" method="post">
             <p>
                 <label>會員名稱</label>
-                <span class="field"><input type="text" name="m_name" class="input-large" value="<?php echo $row['m_name']; ?>" placeholder="請輸入會員名稱"/></span>
+                <span class="field">
+                    <input type="text" name="m_name" class="input-large" value="<?= $Profile['m_name'] ?>"
+                           placeholder="請輸入會員名稱"/>
+                </span>
             </p>
             <p>
                 <label>電子信箱(帳號)</label>
-                <span class="field"><input type="text" name="email" class="input-large" value="<?php echo $row['email']; ?>" placeholder="請輸入電子信箱(帳號)"/></span>
+                <span class="field">
+                    <input type="hidden" name="memberID" value="<?= $Profile['memberID'] ?>">
+                    <input type="text" name="email" class="input-large" value="<?= $Profile['email'] ?>"
+                           placeholder="請輸入電子信箱(帳號)"/>
+                </span>
             </p>
             <p>
                 <label>密碼</label>
-                <span class="field"><input type="password" name="password" class="input-large" value="<?php echo $row['password']; ?>" placeholder="請輸入密碼"/></span>
+                <span class="field">
+                    <input type="password" name="password" class="input-large" placeholder="不更改請留白"/>
+                </span>
             </p>
             <p>
                 <label>身分證字號</label>
-                <span class="field"><input type="text" name="id_card" class="input-large" value="<?php echo $row['id_card']; ?>" placeholder="請輸入身分證字號"/></span>
+                <span class="field">
+                    <input type="text" name="id_card" class="input-large" value="<?= $Profile['id_card']; ?>"
+                           placeholder="請輸入身分證字號"/>
+                </span>
             </p>
             <p>
                 <label>生日</label>
-                <span class="field"><input type="date" name="birthday" class="input-large" value="<?php echo $row['birthday']; ?>" /></span>
+                <span class="field">
+                    <input type="date" name="birthday" class="input-large" value="<?= $Profile['born']; ?>"/>
+                </span>
             </p>
             <p>
                 <label>性別</label>
                 <span class="field">
                     <select name="sex" id="sex" class="uniformselect">
-                        <option value="">請選擇性別</option>
-                        <option <?php if($row['sex'] == 1){echo 'selected';} ?> value="1">男</option>
-                        <option <?php if($row['sex'] == 0){echo 'selected';} ?> value="0">女</option>
+                        <?= $genderOptionsHtml ?>
                     </select>
+                </span>
+            </p>
+            <p>
+                <label>出金帳戶</label>
+                <span class="field">
+                    <select class="form-control" name="bank_id" style="margin: 0px;">
+                        <option value="NaN">請選擇銀行</option>
+                        <?= $bankIDListHtml ?>
+                    </select>
+                    <input class="form-control" name="bank_no" type="text" value="<?= $Profile['bank_no'] ?>"
+                           placeholder="ex：0001990-9876123">
                 </span>
             </p>
             <p>
                 <label>行動電話</label>
-                <span class="field"><input type="text" name="cellphone" class="input-large" value="<?php echo $row['cellphone']; ?>" placeholder="請輸入行動電話"/></span>
+                <span class="field">
+                    <input type="text" name="cellphone" class="input-large" value="<?= $Profile['cellphone'] ?>"
+                           placeholder="請輸入行動電話"/></span>
             </p>
-<!--            <p>-->
-<!--                <label>LINE_ID</label>-->
-<!--                <span class="field"><input type="text" name="line" class="input-large" value="--><?php //echo $row['line']; ?><!--" placeholder="請輸入LINE_ID"/></span>-->
-<!--            </p>-->
             <p>
                 <label>紅利點數</label>
-                <span class="field"><input type="text" name="bonus" class="input-large" value="<?php echo $row['bonus'] == "" ? '0' : $row['bonus']; ?>" placeholder="獲得的紅利點數"/></span>
+                <span class="field">
+                    預留顯示點數，不可於此更動點數
+                </span>
             </p>
             <p>
                 <label>獲得分潤</label>
-                <span class="field"><input type="text" name="profit" class="input-large" value="<?php echo $row['profit'] == "" ? '0' : $row['profit']; ?>" placeholder="獲得的行銷利潤"/></span>
+                <span class="field">
+                    預留顯示分潤，不可於此更動分潤
+                </span>
             </p>
             <p>
                 <label>地址</label>
                 <span class="field">
-                    <select name="city_id" id="city_id" class="uniformselect">
+                    <select name="city" id="city_id" class="form-control">
                         <option value="">請選擇市</option>
-                        <?php
-                        $city_sql = "SELECT * FROM city";
-                        $city_res = mysql_query($city_sql);
-                        while ($city_row = mysql_fetch_array($city_res))
-                        {
-                            if($row['city_id'] == $city_row['id'])
-                            {
-                                echo "<option selected value=".$city_row['id'].">".$city_row['city']."</option>";
-                            }
-                            else
-                            {
-                                echo "<option value=".$city_row['id'].">".$city_row['city']."</option>";
-                            }
-                        }
-                        ?>
+                        <?= $cityOptionsHtml ?>
                     </select>
-                    <select name='area_id' id="area_id" class="uniformselect">
+                    <select name="area" id="area_id" class="form-control">
                         <option value="">請選擇區</option>
-                        <?php
-                        $area_sql = "SELECT * FROM area";
-                        $area_res = mysql_query($area_sql);
-                        while ($area_row = mysql_fetch_array($area_res))
-                        {
-                            if($row['area_id'] == $area_row['id'])
-                            {
-                                echo "<option selected value=".$area_row['id'].">".$area_row['area']."</option>";
-                            }
-                            else
-                            {
-                                echo "<option value=".$area_row['id'].">".$area_row['area']."</option>";
-                            }
-                        }
-                        ?>
-                    </select><br>
-                    <input type="text" name="address" value="<?php echo $row['address']; ?>" placeholder="請輸入地址" class="input-xxlarge"/>
+                        <?= $areaOptionsHtml ?>
+                    </select>
+                    <br>
+                    <input type="text" name="address" value="<?= $Profile['address'] ?>" class=" input-xxlarge"/>
                 </span>
             </p>
             <p class="stdformbutton">
                 <input type="submit" name="btn" class="btn btn-primary span1" value="修改">&nbsp;&nbsp;&nbsp;&nbsp;
-                <input type="button" class="btn btn-default span1" value="返回" onclick="location.href='home.php?url=member'">
+                <input type="button" class="btn btn-default span1" value="返回"
+                       onclick="location.href='home.php?url=member'">
             </p>
         </form>
     </div><!--widgetcontent-->
@@ -119,37 +136,42 @@ $row = mysql_fetch_array($res);
 @$address = $_POST['address'];
 //@$status = $_POST['status'];
 
-if(isset($_POST['btn']))
-{
+if (isset($_POST['btn'])) {
     $sql = "UPDATE member SET m_name='$m_name', email='$email', password='$password', id_card='$id_card', birthday='$birthday', 
  sex='$sex', cellphone='$cellphone', bonus='$bonus', profit='$profit', city_id='$city_id', area_id='$area_id', address='$address' WHERE id='$id'";
     mysql_query($sql);
     ?>
     <script>
         alert('修改成功');
-        location.href='home.php?url=member';
+        location.href = 'home.php?url=member';
     </script>
     <?php
 }
 ?>
 
 <script type="text/javascript">
-    $("#city_id").change(function()
-    {
+
+    $('#profile_form').on('submit', function () {
+        let inputValue = getFormData($(this));
+        ajax17mai('Member', 'UpdateProfile', {}, inputValue);
+        return false;
+    });
+
+    $("#city_id").change(function () {
         $("#area_id").find("option").not(":first").remove();
 
         var id = $(this).val();
         $.ajax
         ({
-            url:"sever_ajax.php", //接收頁
-            type:"POST", //POST傳輸
-            data:{type:'get_area', city_id:id}, // key/value
-            dataType:"text", //回傳形態
-            success:function(i) //成功就....
+            url: "sever_ajax.php", //接收頁
+            type: "POST", //POST傳輸
+            data: {type: 'get_area', city_id: id}, // key/value
+            dataType: "text", //回傳形態
+            success: function (i) //成功就....
             {
                 $("#area_id").append(i);
             },
-            error:function()//失敗就...
+            error: function ()//失敗就...
             {
                 alert("ajax失敗");
             }
