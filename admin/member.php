6255cb57e34cb0d98ebad40ed46252683cbe31a1
Xin-an <u0251081@nkfust.edu.tw>
2018-12-16 16:56:19 +0800

    list of member add member account

--- admin/member.php
+++ admin/member.php
@@ -7,10 +7,11 @@
     <tr>
         <th style="text-align:center;">編號</th>
         <!--<th style="text-align:center;">fb會員id</th>-->
+        <th style="text-align:center;">團購家族-家長</th>
         <th style="text-align:center;">會員帳號</th>
         <th style="text-align:center;">會員姓名</th>
         <th style="text-align:center;">聯絡電話</th>
-        <th style="text-align:center;">地址</th>
+        <!--<th style="text-align:center;">地址</th>-->
         <th style="text-align:center;">操作</th>
     </tr>
     </thead>
@@ -42,9 +43,15 @@
     $start = $total * ($page - 1); // 初始頁數 =每筆頁數*(頁預設數-1)
     $sql = "SELECT * FROM member ORDER BY id DESC LIMIT " . $start . "," . $total;
     $res = mysql_query($sql); //將查詢資料存到 $result 中
-    @$row = mysql_num_rows($res);
+    $row = mysql_num_rows($res);
     for ($i = 1; $i <= $row; $i++) {
         $ary[$i] = mysql_fetch_array($res);
+        $condition = empty($ary[$i]['parent_no']) ? 'false' : 'member_no = \'' . $ary[$i]['parent_no'] . '\'';
+        $sql2 = 'select m_name from member where ' . $condition . ';';
+        $res2 = mysql_query($sql2); //將查詢資料存到 $result 中
+        $row2 = mysql_fetch_array($res2);
+        $ary[$i]['parent_name'] = isset($row2['m_name']) ? $row2['m_name'] : '尚未設定';
+        //$ary[$i]['parent_name'] = $sql2;
     }
 
     for ($i = 1; $i <= $row; $i++) {
@@ -52,22 +59,25 @@
         <tr align="center">
             <td style="width:3%; text-align:center;"><?= $ary[$i]['id'] ?></td>
             <!--<td style="width:5%; text-align:center;"><?= $ary[$i]['fb_id'] === '' ? '' : $ary[$i]['fb_id'] ?></td>-->
+            <td style="width:5%; text-align:center;"><?= $ary[$i]['parent_name'] ?></td>
             <td style="width:5%; text-align:center;"><?= $ary[$i]['email'] ?></td>
             <td style="width:5%; text-align:center;"><?= $ary[$i]['m_name'] ?></td>
             <td style="width:8%;"><?php echo $ary[$i]['cellphone']; ?></td>
+            <!--
             <td style="width:15%">
                 <?php
-                $city_sql = "SELECT city FROM city WHERE id='" . $ary[$i]['city_id'] . "'";
-                $city_res = mysql_query($city_sql);
-                $city_row = mysql_fetch_array($city_res);
+            $city_sql = "SELECT city FROM city WHERE id='" . $ary[$i]['city_id'] . "'";
+            $city_res = mysql_query($city_sql);
+            $city_row = mysql_fetch_array($city_res);
 
-                $area_sql = "SELECT area FROM area WHERE id='" . $ary[$i]['area_id'] . "'";
-                $area_res = mysql_query($area_sql);
-                $area_row = mysql_fetch_array($area_res);
+            $area_sql = "SELECT area FROM area WHERE id='" . $ary[$i]['area_id'] . "'";
+            $area_res = mysql_query($area_sql);
+            $area_row = mysql_fetch_array($area_res);
 
-                echo $city_row['city'] . $area_row['area'] . $ary[$i]['address'];
-                ?>
+            echo $city_row['city'] . $area_row['area'] . $ary[$i]['address'];
+            ?>
             </td>
+            -->
             <td width="13%">
                 <a href="javascript:void(0);" onclick="edit_fun(<?php echo $ary[$i]['id']; ?>)" class="btn"
                    style="color:#fff; background: green;">
